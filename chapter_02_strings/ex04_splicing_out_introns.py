a56c2ab18b1f63fcfb6f3cb8c90000b52824b1b0
biovermell <framirezmontserrat@gmail.com>
2026-08-14 20:59:58 -0600

    feat: add draft answer to chapter 02 exercise 04: splicing out introns

---
+++ chapter_02_strings/ex04_splicing_out_introns.py
@@ -0,0 +1,9 @@
+"""
+Chapter 02, exercise 04: Splicing out introns
+Given a DNA sequence that comprises two exons and an intron, write a program that will:
+- Print just the  coding regions of the DNA sequence
+- Calculate what percentage of the sequence is coding
+- Print out the original genomic DNA sequence with coding bases in uppercase and non-coding bases in lowercase
+"""
+
+seq = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
