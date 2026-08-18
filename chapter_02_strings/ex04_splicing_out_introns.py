784f0003c4811cda8df0b9ccafccb673b17e85e1
biovermell <framirezmontserrat@gmail.com>
2026-08-17 20:24:08 -0600

    feat: add answer to chapter 02 exercise 04: splicing out introns (part 01)

--- chapter_02_strings/ex04_splicing_out_introns.py
+++ chapter_02_strings/ex04_splicing_out_introns.py
@@ -4,6 +4,16 @@ Given a DNA sequence that comprises two exons and an intron, write a program tha
 - Print just the  coding regions of the DNA sequence
 - Calculate what percentage of the sequence is coding
 - Print out the original genomic DNA sequence with coding bases in uppercase and non-coding bases in lowercase
+
+The first exon runs from the start of the sequence to the 63rd character, and the second runs from the 91st character to
+the end of the sequence
 """
 
 seq = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
+
+exon1 = seq[0:63]
+exon2 = seq[90:]
+
+coding_region = exon1 + exon2
+
+print(coding_region)
