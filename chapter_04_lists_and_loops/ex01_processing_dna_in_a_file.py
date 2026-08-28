9be08b86e3bdfc509f289eef9a00da6135fb8ee5
biovermell <framirezmontserrat@gmail.com>
2026-08-27 20:11:07 -0600

    feat: add answer to chapter 04 exercise 01: processing dna in a file

---
+++ chapter_04_lists_and_loops/ex01_processing_dna_in_a_file.py
@@ -0,0 +1,5 @@
+"""
+Given DNA sequences from an input file, all of which start with the same 14 base pair fragment, write a program that will:
+(a) Trim the sequence and and write the cleaned sequences to a new file
+(b) Print the length of each sequence to the screen
+"""
