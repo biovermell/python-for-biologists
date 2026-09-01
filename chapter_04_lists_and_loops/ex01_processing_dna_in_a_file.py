8d0eb1df264384d34e2450aacbc11df0489cf5af
biovermell <framirezmontserrat@gmail.com>
2026-08-31 20:58:37 -0600

    feat: add answer to chapter 04 exercise 01: processing dna in a file

--- chapter_04_lists_and_loops/ex01_processing_dna_in_a_file.py
+++ chapter_04_lists_and_loops/ex01_processing_dna_in_a_file.py
@@ -3,3 +3,14 @@ Given DNA sequences from an input file, all of which start with the same 14 base
 (a) Trim the sequence and and write the cleaned sequences to a new file
 (b) Print the length of each sequence to the screen
 """
+
+with open("input.txt", "r") as infile:
+    for seq in infile:
+        trimmed_seq = seq[14:]
+    with open("output.txt", "w") as outfile:
+        outfile.write(trimmed_seq)
+
+with open("output.txt", "r") as infile:
+    for seq in infile:
+        seq_length = len(seq)
+        print(seq_length)
