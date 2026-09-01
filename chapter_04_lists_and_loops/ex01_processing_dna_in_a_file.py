cffc4b35a7245e1ed43c3f48759540121df8901a
biovermell <framirezmontserrat@gmail.com>
2026-09-01 17:55:35 -0600

    feat: add answer to chapter 04 exercise 01: processing dna in a file

--- chapter_04_lists_and_loops/ex01_processing_dna_in_a_file.py
+++ chapter_04_lists_and_loops/ex01_processing_dna_in_a_file.py
@@ -1,16 +1,13 @@
 """
-Given DNA sequences from an input file, all of which start with the same 14 base pair fragment, write a program that will:
+Given DNA sequences from an input file, all of which start with
+the same 14 base pair fragment, write a program that will:
 (a) Trim the sequence and and write the cleaned sequences to a new file
 (b) Print the length of each sequence to the screen
 """
 
-with open("input.txt", "r") as infile:
+with open("input.txt", "r") as infile, open("output.txt", "w") as outfile:
     for seq in infile:
-        trimmed_seq = seq[14:]
-    with open("output.txt", "w") as outfile:
-        outfile.write(trimmed_seq)
-
-with open("output.txt", "r") as infile:
-    for seq in infile:
-        seq_length = len(seq)
+        trimmed_seq = seq[14:].strip()
+        seq_length = len(trimmed_seq)
         print(seq_length)
+        outfile.write(trimmed_seq + "\n")
