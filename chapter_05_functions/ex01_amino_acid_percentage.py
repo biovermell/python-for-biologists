dfa21d5c8769571692d9e3da990ffe36ac120782
biovermell <framirezmontserrat@gmail.com>
2026-09-07 21:06:01 -0600

    feat: add draft answer to chapter 05 exercise 01: percentage of amino acid residues (part 01)

--- chapter_05_functions/ex01_amino_acid_percentage.py
+++ chapter_05_functions/ex01_amino_acid_percentage.py
@@ -13,16 +13,37 @@ Y and V)
 Use the given assertions to test the function
 """
 
+hydrophobic_aa = ["A", "I", "L", "M", "F", "W", "Y", "V"]
 
-# aa stands for amino acid
-def amino_acid_percentage(prot_seq, aa):
-    aa_count = prot_seq.count(aa.upper())
-    aa_percentage = (aa_count * 100) / len(prot_seq)
+
+# HELPER FUNCTION
+# Iterates through the list of aa and turns them to uppercase
+def uppercase_aa(aa):
+    upper_aa = []
+    for residue in aa:
+        upper_residue = residue.upper()
+        upper_aa.append(upper_residue)
+    return upper_aa
+
+
+# MAIN FUNCTION
+def amino_acid_percentage(prot_seq, aa=hydrophobic_aa):
+    upper_prot_seq = prot_seq.upper()
+    uppercase_aa(aa)
+    residue_count = upper_prot_seq.count(residue)
+    aa_percentage = (residue_count * 100) / len(upper_prot_seq)
     return aa_percentage
 
 
 # GIVEN ASSERTIONS
+# PART ONE
 assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "M") == 5
 assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "r") == 10
 assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "L") == 50
 assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "Y") == 0
+
+# PART TWO
+assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", ["M"]) == 5
+assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", ["M", "L"]) == 55
+assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", ["F", "S", "L"]) == 70
+assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP") == 65
