ef4f260e64d76f5483d95c7a3822a0e33dba8af8
biovermell <framirezmontserrat@gmail.com>
2026-09-09 06:44:48 -0600

    test: add assertions to test helper function

--- chapter_05_functions/ex01_amino_acid_percentage.py
+++ chapter_05_functions/ex01_amino_acid_percentage.py
@@ -25,25 +25,29 @@ def uppercase_aa(aa):
         upper_aa.append(upper_residue)
     return upper_aa
 
-
 # MAIN FUNCTION
-def amino_acid_percentage(prot_seq, aa=hydrophobic_aa):
-    upper_prot_seq = prot_seq.upper()
-    uppercase_aa(aa)
-    residue_count = upper_prot_seq.count(residue)
-    aa_percentage = (residue_count * 100) / len(upper_prot_seq)
-    return aa_percentage
+#def amino_acid_percentage(prot_seq, aa=hydrophobic_aa):
+#    upper_prot_seq = prot_seq.upper()
+#    uppercase_aa(aa)
+#    residue_count = upper_prot_seq.count(residue)
+#    aa_percentage = (residue_count * 100) / len(upper_prot_seq)
+#    return aa_percentage
 
 
 # GIVEN ASSERTIONS
 # PART ONE
-assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "M") == 5
-assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "r") == 10
-assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "L") == 50
-assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "Y") == 0
+#assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "M") == 5
+#assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "r") == 10
+#assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "L") == 50
+#assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", "Y") == 0
 
 # PART TWO
-assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", ["M"]) == 5
-assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", ["M", "L"]) == 55
-assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", ["F", "S", "L"]) == 70
-assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP") == 65
+#assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", ["M"]) == 5
+#assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", ["M", "L"]) == 55
+#assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP", ["F", "S", "L"]) == 70
+#assert amino_acid_percentage("MSRSLLLRFLLFLLLLPPLP") == 65
+
+# ASSERTIONS FOR HELPER FUNCTION
+assert uppercase_aa(["A", "B", "C"]) == ["A", "B", "C"]
+assert uppercase_aa(["A", "b", "C"]) == ["A", "B", "C"]
+assert uppercase_aa(["a", "b", "c"]) == ["A", "B", "C"]
