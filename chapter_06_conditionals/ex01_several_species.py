159fcc799c36a48127ea43151a27acb1b853c1f2
biovermell <framirezmontserrat@gmail.com>
2026-09-11 03:33:51 -0600

    feat: add answer file for chapter 06 exercise 01: several species

---
+++ chapter_06_conditionals/ex01_several_species.py
@@ -0,0 +1,6 @@
+"""
+Chapter 06, exercise 01: Several species
+Given a csv file containing made-up data for a number of genes,
+Print out the gene names for all genes belonging to Drosophila melanogaster or
+Drosophila simulans
+"""
