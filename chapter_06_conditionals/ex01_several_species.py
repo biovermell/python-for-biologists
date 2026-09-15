94c915736c418824c17d065d6ab27a2a6d9ca598
biovermell <framirezmontserrat@gmail.com>
2026-09-14 22:20:23 -0600

    feat: add answer to chapter 06 exercise 01: several species

--- chapter_06_conditionals/ex01_several_species.py
+++ chapter_06_conditionals/ex01_several_species.py
@@ -1,6 +1,13 @@
 """
 Chapter 06, exercise 01: Several species
-Given a csv file containing made-up data for a number of genes,
-Print out the gene names for all genes belonging to Drosophila melanogaster or
-Drosophila simulans
+Given a .csv file containing made-up data for a number of genes,
+print out the gene names for all genes belonging to Drosophila
+melanogaster or Drosophila simulans
 """
+
+with open("data.csv", "r") as infile:
+    for line in infile:
+        if line.startswith(("Drosophila melanogaster", "Drosophila simulans")):
+            data = line.strip().split(",")
+            gene_names = data[2]
+            print(gene_names)
