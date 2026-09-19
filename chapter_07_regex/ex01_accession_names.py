67f4164b3bae93880ab72552954c337ca4d24924
biovermell <framirezmontserrat@gmail.com>
2026-09-18 21:07:47 -0600

    feat: add draft answer to chapter 07 exercise 01: accession names

---
+++ chapter_07_regex/ex01_accession_names.py
@@ -0,0 +1,28 @@
+"""
+Chapter 07, exercise 01: Accession names
+Given a list of made-up gene accession names, write a
+program that will print only the accession names that
+satisfy the following criteria – treat each criterion separately:
+a) contain the number 5
+b) contain the letter d or e
+c) contain the letters d and e in that order
+d) contain the letters d and e in that order with a single letter between them
+e) contain both the letters d and e in any order
+f) start with x or y
+g) start with x or y and end with e
+h) contain three or more numbers in a row
+i) end with d followed by either a, r or p
+"""
+
+import re
+
+accession_names = [
+    "xkn59438",
+    "yhdck2",
+    "eihd39d9",
+    "chdsye847",
+    "hedle3455",
+    "xjhd53e",
+    "45da",
+    "de37dp",
+]
